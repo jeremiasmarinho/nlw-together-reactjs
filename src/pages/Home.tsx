658f9d8dc235b216d27941f2062367fbb8de4b4b
Jeremias Marinho <jeremias10@msn.com>
2021-10-18 01:39:08 -0300

    criando as autenticações com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
+
+import { useHistory } from 'react-router-dom';
+
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
-import { Button } from '../components/Button'
+import { Button } from '../components/Button';
+
+
 
 export function Home () {
+  const history = useHistory();
+
+
+
+  function handleCreateRoom() {
+      history.push('/rooms/new');
+ 
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -14,9 +30,10 @@ export function Home () {
         <p>Tire as dúvidas da sua audiência em tempo real.</p>
       </aside>
       <main>
+        
         <div className="main-content">
           <img src={logoImg} alt="logo" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
 
+
+
 export function NewRoom () {
+
   return (
     <div id="page-auth">
       <aside>
@@ -14,6 +17,7 @@ export function NewRoom () {
         <p>Tire as dúvidas da sua audiência em tempo real.</p>
       </aside>
       <main>
+        
         <div className="main-content">
           <img src={logoImg} alt="logo" />
           <h2>Criar uma nova sala</h2>
@@ -27,7 +31,7 @@ export function NewRoom () {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
